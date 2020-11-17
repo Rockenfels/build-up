aa1df0a5a7aab2444e6314d1fc0dfc984635d8f6
Ray O <52436369+Rockenfels@users.noreply.github.com>
2020-11-17 11:27:30 -0700

    File structure cleanup

--- backend/build-up-api/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  resources :categories, only: [:show, :index, :create]
-  resources :experiences, only: [:show, :index, :create]
-  get '/newest', to: 'experiences#newest'
-  get  '/most_liked', to: 'experiencs#mostLiked'
-  post '/categories_search', to: 'categories#categories_search'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
